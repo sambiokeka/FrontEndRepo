63630c5a5bb1292a7294ae9c4b4ec12fee7952bc
matheusegea <matheusegea220504@gmail.com>
2025-05-13 18:28:08 -0300

    adicionando js em vinhos

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,14 @@ document.querySelectorAll('.conteudo_home .img_border').forEach((img) => {
     img.classList.add('not-hovered'); 
   });
 });
+document.querySelectorAll('.vinho-grid .vinho-card img').forEach((img) => {
+  img.addEventListener('mouseover', () => {
+    img.classList.remove('not-hovered'); 
+    img.classList.add('hovered'); 
+  });
+
+  img.addEventListener('mouseout', () => {
+    img.classList.remove('hovered'); 
+    img.classList.add('not-hovered'); 
+  });
+});
